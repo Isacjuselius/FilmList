c92ae5b5ab7adec489b2d75e6ca838550396087c
isacj <id23ias@cs.umu.se>
2026-02-22 14:38:27 +0100

    added validation on filmtitel and genreid

--- Models/FilmDetails.cs
+++ Models/FilmDetails.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FilmList.Models
 {
     public class FilmDetails
     {
         public int FilmId { get; set; }
+
+        [Required(ErrorMessage = "Film title is required.")]
         public string FilmTitle { get; set; }
+
+        [Required(ErrorMessage = "Genre ID is required.")]
         public int GenreId { get; set; }
 
         public FilmDetails(string filmTitle, int filmId, int genreId)
